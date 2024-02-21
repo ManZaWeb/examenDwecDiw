721c8c6a62caeb4502b7fae22eeef43cc28b573c
Mario <maalnu93@hotmail.com>
2024-02-21 10:04:35 +0100

    Iniciar app

--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,22 @@
 //Autor: Mario Alfonso Nuñez
 //GitHub Link: 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    iniciarApp();
+  });
+   
+
+
+  //Funcion que inicia la app y sus funciones.
+  function iniciarApp() {
+    
+    crearGaleria();
+   
+  }
+
+  function crearGaleria(){
+
+
+  }
+   
